ba0f27bd4fa3bbd5f6c0ebe577885e715b765f23
Duncan Leo <hello@duncanleo.me>
2019-10-05 19:19:12 +0800

    add functional scraping

--- cmd/scrape/scrape.go
+++ cmd/scrape/scrape.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 
 	"github.com/duncanleo/brawl-scraper/brawl"
+	"github.com/duncanleo/brawl-scraper/db"
+	"github.com/duncanleo/brawl-scraper/model"
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -16,10 +18,42 @@ func main() {
 	brawlClient := brawl.NewClient(os.Getenv("BRAWL_API_TOKEN"))
 
 	for _, brawlerTag := range brawlers {
-		x, err := brawlClient.GetPlayerData(brawlerTag)
+		playerStats, err := brawlClient.GetPlayerData(brawlerTag)
 		if err != nil {
 			log.Println(err)
+			continue
 		}
-		log.Printf("%+v\n", x)
+		// Upsert player object
+		var player model.Player
+		err = db.DB.
+			Where(model.Player{Tag: brawlerTag}).
+			Assign(model.Player{
+				Name:      playerStats.Name,
+				NameColor: playerStats.NameColor,
+				Tag:       brawlerTag,
+			}).
+			FirstOrCreate(&player).
+			Error
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		// Create player data
+		var playerData = model.PlayerData{
+			Player:               player,
+			TrophyCount:          playerStats.Trophies,
+			ExpLevel:             playerStats.ExpLevel,
+			ExpPoints:            playerStats.ExpPoints,
+			ThreeV3Victories:     playerStats.ThreeVs3Victories,
+			SoloVictories:        playerStats.SoloVictories,
+			DuoVictories:         playerStats.DuoVictories,
+			BestRoboRumbleTime:   playerStats.BestRoboRumbleTime,
+			BestTimeAsBigBrawler: playerStats.BestTimeAsBigBrawler,
+		}
+		err = db.DB.Create(&playerData).Error
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 	}
 }
--- model/model.go
+++ model/model.go
@@ -9,10 +9,27 @@ type Player struct {
 	UpdatedAt time.Time  `json:"updated_at"`
 	DeletedAt *time.Time `sql:"index" json:"-"`
 	Name      string     `gorm:"name" json:"name"`
+	NameColor string     `gorm:"name_color" json:"name_color"`
+	Tag       string     `gorm:"tag" json:"tag"`
 }
 
 // PlayerData represents the state of a player at a given time
 type PlayerData struct {
+	ID                   uint       `gorm:"primary_key"`
+	CreatedAt            time.Time  `json:"created_at"`
+	UpdatedAt            time.Time  `json:"updated_at"`
+	DeletedAt            *time.Time `sql:"index" json:"-"`
+	Player               Player     `json:"player"`
+	PlayerID             uint       `json:"-"`
+	TrophyCount          int        `json:"trophy_count"`
+	ExpLevel             int        `json:"exp_level"`
+	ExpPoints            int        `json:"exp_points"`
+	ThreeV3Victories     int        `json:"3v3_victories"`
+	SoloVictories        int        `json:"solo_victories"`
+	DuoVictories         int        `json:"duo_victories"`
+	BestRoboRumbleTime   int        `json:"best_robo_rumble_time"`
+	BestTimeAsBigBrawler int        `json:"best_time_as_big_brawler"`
+}
 	ID        uint       `gorm:"primary_key"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
