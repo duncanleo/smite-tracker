5cf1b51f001129b3f6fc4952cd975d6b1032d912
Duncan Leo <hello@duncanleo.me>
2019-10-05 21:13:37 +0800

    add optional support for Fixie

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/duncanleo/brawl-scraper/db"
 	"github.com/duncanleo/brawl-scraper/web"
 	"github.com/gin-gonic/gin"
@@ -8,6 +10,11 @@ import (
 )
 
 func main() {
+	// Fixie support
+	if len(os.Getenv("FIXIE_URL")) > 0 {
+		os.Setenv("HTTP_PROXY", os.Getenv("FIXIE_URL"))
+		os.Setenv("HTTPS_PROXY", os.Getenv("FIXIE_URL"))
+	}
 	db.DB.LogMode(gin.IsDebugging())
 	web.StartServer()
 }
